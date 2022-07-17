a429e74daf2e7e4c2309882fe7b534a105f8c069
Elberd Chahkiev <takututu95@gmail.com>
2022-07-17 17:18:20 +0300

    fin lend without text

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
 
+const feedback_slider = new Swiper('.feedback-slider', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+})
+
+const cards_slider = new Swiper('.cards--sllider', {
+    effect: 'cards', 
+    grabCursor: true,
+})
+
 
 const menu = document.querySelector('#main_menu')
 const triggers = document.querySelectorAll('.open-close')
@@ -10,17 +25,40 @@ for (let index = 0; index < triggers.length; index++) {
 }
 
 
-// let cardNum = '1234567891234567'
-// let cardHiddenNum = []
 
-// const getHiddenCard = (card, index) => {
-//     card.split('')
-//     return card[index] = '*'
-// }
+AOS.init()
+AOS.init({
+    disable: false,
+    startEvent: 'DOMContentLoaded',
+    initClassName: 'aos-init', 
+    animatedClassName: 'aos-animate', 
+    useClassNames: false, 
+    disableMutationObserver: false, 
+    debounceDelay: 50, 
+    throttleDelay: 99,
 
-// for (let index = 0; index < cardNum.length; index++) {
-//     if(index < 12) cardHiddenNum.push(getHiddenCard(cardNum, index)) 
-//     else cardHiddenNum.push(cardNum[index])
-// }
 
-// console.log(cardHiddenNum.join(''))
+    offset: 120,
+    delay: 0, 
+    duration: 400,
+    easing: 'ease', 
+    once: false,
+    mirror: false,
+    anchorPlacement: 'top-bottom', 
+})
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
+
+
+
